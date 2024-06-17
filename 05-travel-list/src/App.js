eb2c2dc4816b4b961fdcbfa12d39fc341572f290
werzizu <werzizu@gmail.com>
2024-06-17 17:43:28 +0200

    sorting by input, name, packed and clear all items

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -109,10 +109,31 @@ function Form({ initialItems, onAddItems }) {
 }
 
 function PackingList({ initialItems, setInitialItems, onDeleteItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = initialItems;
+  if (sortBy === "desctiption")
+    sortedItems = initialItems
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = initialItems
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  function onClearAllItems() {
+    const confirm = window.confirm("Are you really want to clear all items?");
+    if (confirm) {
+      setInitialItems([]);
+    }
+  }
+
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -121,6 +142,16 @@ function PackingList({ initialItems, setInitialItems, onDeleteItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort By Input Order </option>
+          <option value="desctiption">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+
+        <button onClick={onClearAllItems}>Clear List</button>
+      </div>
     </div>
   );
 }
